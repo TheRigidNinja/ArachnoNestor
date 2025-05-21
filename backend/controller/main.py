d8a3f1046cac594e0c61052c3552b2acfd5fbe26
Brian Shisanya <brian.shisanya@example.com>
2025-05-21 00:55:41 +0000

    Re-naming python env file

--- backend/controller/main.py
+++ backend/controller/main.py
@@ -1,9 +1,13 @@
 from controller import ArachnoNestor
-
+from time import sleep
 if __name__=='__main__':
-    bot = ArachnoNestor([1,2,3,4])
+    bot = ArachnoNestor([1])
     # example: move all motors at 1500 RPM forward:
-    bot.engage_motors(1500, forward=True)
+    bot.engage_motors(100, forward=True)
     # ... your logic here ...
     # then stop:
+
+
+    sleep(5)
     bot.stop_all(brake=False)
+    # bot.stop_all(brake=True)
